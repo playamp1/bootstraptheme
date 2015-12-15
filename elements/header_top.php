57afb9602f011be5b216565bdb5c21319a323d8f
acliss19xx <acliss19xx@gmail.com>
2015-12-15 21:50:08 +0900

    fix delete main.less
    main.lessはなく、リンクエラー担っているので削除

--- elements/header_top.php
+++ elements/header_top.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link href="<?=$view->getStylesheet('bootstrap.less')?>" rel='stylesheet' type='text/css'>
 <!-- 	<link rel="stylesheet" type="text/css" href="<?php echo $view->getThemePath()?>/css/bootstrap.css"> -->
-    <?php echo $html->css($view->getStylesheet('main.less'))?>
     <?php Loader::element('header_required', array('pageTitle' => isset($pageTitle) ? $pageTitle : ''));?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script>
